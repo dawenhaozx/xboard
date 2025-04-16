fdd8e2d31fba0107aea943afbb2c41fab043f6fa
dawenhaozx <58341754+dawenhaozx@users.noreply.github.com>
2025-04-16 14:33:53 +0800

    add subscribe_path
    (cherry picked from commit 378ac1c450d1a5ed101306ae25c57a547ed954da)

--- app/Http/Routes/V1/ClientRoute.php
+++ app/Http/Routes/V1/ClientRoute.php
@@ -12,7 +12,10 @@ class ClientRoute
             'middleware' => 'client'
         ], function ($router) {
             // Client
-            $router->get('/subscribe', 'V1\\Client\\ClientController@subscribe')->name('client.subscribe');
+            if (empty(admin_setting('subscribe_path'))) {
+                // 只有后台没配置自定义路径，才使用默认 /subscribe
+                $router->get('/subscribe', 'V1\Client\ClientController@subscribe')->name('client.subscribe');
+            }
             // App
             $router->get('/app/getConfig', 'V1\\Client\\AppController@getConfig');
             $router->get('/app/getVersion', 'V1\\Client\\AppController@getVersion');
